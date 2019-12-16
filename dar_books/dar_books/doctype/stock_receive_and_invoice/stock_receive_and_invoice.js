4c8ce25dc9fba27487eb26e8279c40821948621c
ashish-greycube <mr.ashish.shah@gmail.com>
2019-12-16 17:28:27 +0530

    first draft for Stock Entry and SI

--- dar_books/dar_books/doctype/stock_receive_and_invoice/stock_receive_and_invoice.js
+++ dar_books/dar_books/doctype/stock_receive_and_invoice/stock_receive_and_invoice.js
@@ -1,100 +1,93 @@
 // Copyright (c) 2019, GreyCube Technologies and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Stock Receive and Invoice', { 
-	onload(frm)
-	{
-	// alert("on load");
-	// on form load set posting_time as now
-	if(frm.docstatus = 0)
-	{
-	frm.set_value("posting_time",frappe.datetime.now_time());
-	}	
+frappe.ui.form.on('Stock Receive and Invoice', {
+	onload(frm) {
+		// alert("on load");
+		// on form load set posting_time as now
+		if (frm.docstatus = 0) {
+			frm.set_value("posting_time", frappe.datetime.now_time());
+		}
 	},
-	validate(frm)
-		{
-		if(frm.docstatus = 0)
-		{
-		// to check if there is single sales invoice then sales fields are manadatory
-		//alert("validate ");
-		var sales_exist = false;
-		$.each(frm.doc.items || [], function(i, d) {
-			
-				if(d.sold_qty > 0 ) 
-				{
+	validate(frm) {
+		if (frm.docstatus = 0) {
+			// to check if there is single sales invoice then sales fields are manadatory
+			//alert("validate ");
+			var sales_exist = false;
+			$.each(frm.doc.items || [], function (i, d) {
+
+				if (d.sold_qty > 0) {
 					//alert("sales_item_exist");
-						//check if sales invoice will be created 
-						sales_exist =true;
-						return false;
-					}
-					else
-					{
-						//nothing
-					}
-					
-			  });
-			  		frm.set_df_property("sales_taxes_and_charges_template","reqd",true);
-					frm.set_df_property("cost_center","reqd",true);
-					if(frm.sales_taxes_and_charges_template && frm.cost_center)
-					{
-						// frappe.validated=true;
-						return true;
-					}
-					{
-						// frappe.validated=false;
-						frm.refresh_field("cost_center");
-						frm.refresh_field("sales_taxes_and_charges_template");
-						return false;
-
-					}
-
-		}
-		},
-		fetch_customer_warehouse_items(frm){
-			frappe.call({
-				method:"dar_books.api.get_items",
-				args: {
-					warehouse: frm.doc.customer_warehouse,
-					posting_date: frm.doc.posting_date,
-					posting_time: frm.doc.posting_time,
-					company:frm.doc.company
-				},
-				callback: function(r) {
-					//console.log(r)
-					var items = [];
-					frm.clear_table("items");
-					for(var i=0; i< r.message.length; i++) {
-						var d = frm.add_child("items",
-						{item:r.message[i].item_code, customer_qty:r.message[i].current_qty, left_qty :0 , sold_qty:r.message[i].current_qty});
-						$.extend(d, r.message[i]);
-					}
-					frm.refresh_field("items");
+					//check if sales invoice will be created 
+					sales_exist = true;
+					return false;
+				} else {
+					//nothing
 				}
-		
+
 			});
+			frm.set_df_property("sales_taxes_and_charges_template", "reqd", true);
+			frm.set_df_property("cost_center", "reqd", true);
+			if (frm.sales_taxes_and_charges_template && frm.cost_center) {
+				// frappe.validated=true;
+				return true;
+			} {
+				// frappe.validated=false;
+				frm.refresh_field("cost_center");
+				frm.refresh_field("sales_taxes_and_charges_template");
+				return false;
+
+			}
+
 		}
-	});
+	},
+	fetch_customer_warehouse_items(frm) {
+		frappe.call({
+			method: "dar_books.api.get_items",
+			args: {
+				warehouse: frm.doc.customer_warehouse,
+				posting_date: frm.doc.posting_date,
+				posting_time: frm.doc.posting_time,
+				company: frm.doc.company
+			},
+			callback: function (r) {
+				//console.log(r)
+				var items = [];
+				frm.clear_table("items");
+				for (var i = 0; i < r.message.length; i++) {
+					var d = frm.add_child("items", {
+						item: r.message[i].item_code,
+						customer_qty: r.message[i].current_qty,
+						left_qty: 0,
+						sold_qty: r.message[i].current_qty
+					});
+					$.extend(d, r.message[i]);
+				}
+				frm.refresh_field("items");
+			}
+
+		});
+	}
+});
 
 
 frappe.ui.form.on('Stock Receive and Invoice Items',
-    "left_qty", function(frm, cdt, cdn) { // notice the presence of cdt and cdn
-    // that means that child doctype and child docname are passed to function and hence you can know what 
-   // row was modified and triggered
-    var item = locals[cdt][cdn]; // this is where the magic happens
-    // locals is a global array that contains all the local documents opened by the user
-    // item is the row that the user is working with
-    // to what you need to do and update it back
-    var sold_qty = item.customer_qty - item.left_qty ;
-    if (sold_qty < 0)
-    {
-        alert("Left Quantity " + item.left_qty.toString() + " cannot be more than initial Customer Qty "+ item.customer_qty.toString() +" for Item : " + item.item_code);
-        item.left_qty = item.customer_qty;
-        item.sold_qty = 0;
-    }
-    else
-    {
-    item.sold_qty = sold_qty; // remember to refresh the field to see the changes in the UI'
-    }
-    frm.refresh_field("items");
-    }
+	"left_qty",
+	function (frm, cdt, cdn) { // notice the presence of cdt and cdn
+		// that means that child doctype and child docname are passed to function and hence you can know what 
+		// row was modified and triggered
+		var item = locals[cdt][cdn]; // this is where the magic happens
+		// locals is a global array that contains all the local documents opened by the user
+		// item is the row that the user is working with
+		// to what you need to do and update it back
+		var sold_qty = item.customer_qty - item.left_qty;
+		if (sold_qty < 0) {
+			alert("Left Quantity " + item.left_qty.toString() + " cannot be more than initial Customer Qty " + item.customer_qty.toString() + " for Item : " + item.item_code);
+			item.left_qty = item.customer_qty;
+			item.sold_qty = 0;
+		} else {
+			item.sold_qty = sold_qty; // remember to refresh the field to see the changes in the UI'
+		}
+		frm.refresh_field("items");
+	}
 );
--- dar_books/dar_books/doctype/stock_receive_and_invoice/stock_receive_and_invoice.py
+++ dar_books/dar_books/doctype/stock_receive_and_invoice/stock_receive_and_invoice.py
@@ -6,56 +6,73 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from erpnext.utilities.product import get_price
+from frappe.model.mapper import get_mapped_doc
 
 class StockReceiveandInvoice(Document):
 	def on_submit(self):
-		
-		# Sales Invoice
-		si = frappe.get_doc({
-				"doctype": "Sales Invoice",
-				"title": self.customer,
-				"status": "Draft",
-				"comnpany" : self.company,
-				"customer" :self.customer,
-				"posting_date" :self.posting_date,
-				"posting_time": self.posting_time,
-				"source_warerhouse" :self.customer_warehouse,
-				"price_list" : frappe.db.get_value("Selling Settings", None, "selling_price_list"),
-				"cost_center" : self.cost_center,
-				"taxes_and_charges" :self.sales_taxes_and_charges_template
-				
+		print(self.docstatus,'-------')
+		stockentry=self.make_stock_entry(source_name=self.name)
+		doclist=self.make_sales_invoice(source_name=self.name)
 
-			})
 
-		for item in self.get('items',{'sold_qty' : '> 0'}): #get rows with sold_qty > 0 for invoice
-			si.append("items", { 
-				"item_code": item.item,
-				"item_name" :item.item,
-				"qty" :item.sold_qty,
-				"rate" : get_price(item.item,frappe.db.get_value("Selling Settings", None, "selling_price_list"),frappe.get_value("Customer", self.customer, "customer_group"),self.company,item.sold_qty),
-				"description": frappe.get_value("Item", item.item, "description"),
-				"stock_uom" : frappe.get_value("Item", item.item, "stock_uom")
-				
-			})
-		
-			
-		# doc.save()
-		# si.validate()
-		#si.save(ignore_permissions = True)
-		si.insert(ignore_permissions = True)
-		# si.run_method("set_missing_values")
-		
-		# Stock Entry
-		# se = frappe.get_doc({
-		# 		"doctype": "Stock Entry",
-		# 		"purpose": "Material Transfer",
-		# 		"source_warehouse" : self.customer_warehouse,
-		# 		"target_warehouse" : self.agent_warehouse,
-		# 		"title": "Material Transfer from " + self.customer,
-		# 		"status": "Draft"
-		# 	})
-		# # se.append(self.items)
-		# se.insert()
-		# se.save()
+	def make_sales_invoice(self,source_name, target_doc=None, ignore_permissions=True):
 
-	pass
+		def set_missing_values(source, target):
+			target.update_stock=1
+			target.set_warehouse=source.customer_warehouse
+			target.taxes_and_charges=source.sales_taxes_and_charges_template
+			target.flags.ignore_permissions = ignore_permissions
+			target.cost_center =source.cost_center
+			target.run_method("set_missing_values")
+			target.run_method("calculate_taxes_and_totals")
+
+		def update_item(source, target, source_parent):
+			target.item_code=source.item
+			target.qty=source.sold_qty
+		doclist = get_mapped_doc("Stock Receive and Invoice", source_name, {
+				"Stock Receive and Invoice": {
+					"doctype": "Sales Invoice",
+					"validation": {
+						"docstatus": ["=", 1]
+					}
+				},
+				"Stock Receive and Invoice Items": {
+					"doctype": "Sales Invoice Item",
+					"postprocess": update_item,
+					"condition": lambda doc: (doc.sold_qty>0)
+				},
+				"Sales Taxes and Charges": {
+					"doctype": "Sales Taxes and Charges",
+					"add_if_empty": True
+				},				
+			}, target_doc, set_missing_values, ignore_permissions=ignore_permissions)
+		doclist.flags.ignore_mandatory = True
+		doclist.save(ignore_permissions=True)
+		return doclist			
+
+	def make_stock_entry(self,source_name, target_doc=None, ignore_permissions=True):
+		def set_missing_values(source, target):
+			target.purpose='Material Transfer'
+			target.from_warehouse=source.customer_warehouse
+			target.to_warehouse=source.target_warehouse
+			target.flags.ignore_permissions = ignore_permissions
+			target.run_method("set_missing_values")
+		def update_item(source, target, source_parent):
+			target.item_code=source.item
+			target.qty=source.left_qty
+		doclist = get_mapped_doc("Stock Receive and Invoice", source_name, {
+				"Stock Receive and Invoice": {
+					"doctype": "Stock Entry",
+					"validation": {
+						"docstatus": ["=", 1]
+					}
+				},
+				"Stock Receive and Invoice Items": {
+					"doctype": "Stock Entry Detail",
+					"postprocess": update_item,
+					"condition": lambda doc: (doc.left_qty>0)
+				},
+			}, target_doc, set_missing_values, ignore_permissions=ignore_permissions)
+		doclist.flags.ignore_mandatory = True
+		doclist.save(ignore_permissions=True)
+		return doclist		
